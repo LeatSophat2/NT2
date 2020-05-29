084eded45e8061c416b4ad05793470b31512ab6e
LeatSophat2 <leatsophat02@gmail.com>
2020-05-29 14:32:36 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
     <title>Document</title>
 </head>
 <style>
@@ -118,7 +123,7 @@ hr {
 </style>
 <body>
     
-<h2>Modal Signup Form</h2>
+<h2></h2>
 
 <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign Up</button>
 
