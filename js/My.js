265cd2e11c602a3089214faccdb02ffd88e8c006
LeatSophat2 <leatsophat02@gmail.com>
2020-06-01 19:58:16 +0700

    Update My.js

--- js/My.js
+++ js/My.js
@@ -40,7 +40,19 @@ function myTimer() {
 }
 
 // ========================================= Menu ===============================================//
-
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinkPage");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
 
 
 
