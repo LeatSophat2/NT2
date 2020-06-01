1f6d12aeb44b9e98352a159b94d636589b74616b
LeatSophat2 <leatsophat02@gmail.com>
2020-06-01 15:57:02 +0700

    Update Login.php

--- View/Login.php
+++ View/Login.php
@@ -132,7 +132,7 @@ span.psw {
 <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
 
 <div id="id01" class="modal">
-  
+
   <form class="modal-content animate" action="/action_page.php" method="post">
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
@@ -154,7 +154,7 @@ span.psw {
 
     <div class="container" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
+      <span class="psw">Forgot<a href="#">password?</a></span>
     </div>
   </form>
 </div>
@@ -162,7 +162,6 @@ span.psw {
 <script>
 // Get the modal
 var modal = document.getElementById('id02');
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
